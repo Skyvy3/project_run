f8bd4471f59f65a0b4647e42055b0333386fba98
arseny <arsenygolovin@gmail.com>
2025-10-15 14:43:36 +0300

    3z commit

--- app_run/views.py
+++ app_run/views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from urllib3 import request
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from .models import Run
 from rest_framework import viewsets
@@ -27,7 +27,7 @@ class RunViewSet(viewsets.ModelViewSet):
 #
 
 class Users(ReadOnlyModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
     serializer_class = UsersSerializers
     def get_queryset(self):
         queryset = User.objects.filter(is_superuser=False)
