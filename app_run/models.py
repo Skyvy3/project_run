ffaf8b7e1d1f5bb03f0741d0b553e0f5cd6f2df1
arseny <arsenygolovin@gmail.com>
2025-10-15 11:10:03 +0300

    2z commit

--- app_run/models.py
+++ app_run/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+#Mодель забега
 class Run(models.Model):
     athlete = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.TextField(blank=True)
