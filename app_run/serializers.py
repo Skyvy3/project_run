75117f2a07149e19f70cf43e4c3fccfb17c9375a
arseny <arsenygolovin@gmail.com>
2025-10-15 11:12:04 +0300

    2z commit

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -4,4 +4,4 @@ from .models import Run
 class RunSerializer(serializers.ModelSerializer):
     class Meta:
         model = Run
-        fields = ['athlete', 'comment', 'created_at']
+        fields = ['id','athlete', 'comment', 'created_at']
